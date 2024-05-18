43aa8597af4bd43775dc402d9ab1767be4fda1e9
Aasia <missasiakhalid@gmail.com>
2024-05-18 19:56:16 +0500

    added the header component and updated my route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Profile from "./Pages/Profile"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import About from "./Pages/About"
+import Header from "./components/Header"
 
 const App = () => {
   return (
-    <div className="text-blue-500">HI</div>
+ <BrowserRouter>
+ <Header/>
+<Routes>
+  
+  <Route path="/home" element= {<Home />}/>
+  <Route path="/profile" element= {<Profile />}/>
+  <Route path="/sign-in" element= {<SignIn />}/>
+  <Route path="/sign-up" element= {<SignUp />}/>
+  <Route path="/about" element= {<About />}/>
+  
+</Routes>
+ </BrowserRouter>
   )
 }
 
